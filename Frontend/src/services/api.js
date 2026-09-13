4dfdf8f0de36704e6d12c029baed34454e721d29
estebansolera77 <estebansolera77@gmail.com>
2026-09-13 02:49:25 -0500

    fix backend url

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://agrotraz-backend.onrender.com';
+const BASE_URL = 'https://agrotraz-backend.onrender.com/api';
 
 function getToken() {
   return localStorage.getItem('token');
